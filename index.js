7908ca30608fc5200d868485ddf4378e2415e825
AndrefGuirro <131619465+AndrefGuirro@users.noreply.github.com>
2024-09-20 00:24:27 -0300

    .
    .

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-// Função para filtrar as linhas preenchidas
-function filtrarLinhas() {
-    const rows = document.querySelectorAll("tbody tr");
-    rows.forEach(row => {
-        const estoque = row.cells[1].innerText.trim();
-        const sugestaoPedido = row.cells[2].innerText.trim();
-        if (estoque === "" && sugestaoPedido === "") {
-            row.classList.add("hidden");
-        }
-    });
-}
-
-// Função para mostrar todas as linhas
-function mostrarTodasLinhas() {
-    const rows = document.querySelectorAll("tbody tr");
-    rows.forEach(row => {
-        row.classList.remove("hidden");
-    });
-    // Restaurar o texto do botão para "Grupo"
-    document.getElementById('dropdownMenuButton').textContent = "Grupo";
-}
-
-// Função para filtrar por grupo de embalagem e atualizar o texto do botão
+// Função para filtrar os produtos por grupo
 function filtrarPorGrupo(grupo) {
     const rows = document.querySelectorAll("tbody tr");
     rows.forEach(row => {
@@ -30,11 +8,29 @@ function filtrarPorGrupo(grupo) {
             row.classList.add("hidden");
         }
     });
-    // Atualizar o texto do botão para o grupo selecionado
+    // Atualiza o nome do botão com o grupo selecionado
     document.getElementById('dropdownMenuButton').textContent = grupo;
 }
 
-// Adiciona evento ao botão "Todos produtos" para restaurar o texto do dropdown
-document.querySelector('.btn-secondary').addEventListener('click', function() {
+// Função para mostrar todos os produtos
+function mostrarTodasLinhas() {
+    const rows = document.querySelectorAll("tbody tr");
+    rows.forEach(row => {
+        row.classList.remove("hidden");
+    });
     document.getElementById('dropdownMenuButton').textContent = "Grupo";
-});
+}
+
+// Função para filtrar apenas as linhas onde há produtos cadastrados
+function filtrarLinhas() {
+    const rows = document.querySelectorAll("tbody tr");
+    rows.forEach(row => {
+        const estoque = row.cells[1].textContent.trim();
+        const sugestao = row.cells[2].textContent.trim();
+        if (estoque || sugestao) {
+            row.classList.remove("hidden");
+        } else {
+            row.classList.add("hidden");
+        }
+    });
+}
